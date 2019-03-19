4faf786b19cfc8156fe791614632d83fa4455bd6
planetoneautomation <48492716+planetoneautomation@users.noreply.github.com>
2019-03-19 16:50:37 -0400

    Oh wait ....

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("The WebHook Triggered a Jenkins Build - Really? With ngrok.exe")
+        MsgBox("The WebHook Triggered a Jenkins Build - Really? With ngrok.exe for forwarding.")
         Close()
     End Sub
 End Class
