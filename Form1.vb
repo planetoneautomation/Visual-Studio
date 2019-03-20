5ea0a6aa5637916af56d6312997a7bbb319346a3
planetoneautomation <48492716+planetoneautomation@users.noreply.github.com>
2019-03-20 14:23:12 -0400

    Changing label text

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'MsgBox("GitHub Commit Triggered Visual Studio Build in Jenkins")
         Close()
     End Sub
 End Class
