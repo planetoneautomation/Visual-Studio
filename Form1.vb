882f7444d3de482b0cbc2007a148a996d6b1fcdd
planetoneautomation <48492716+planetoneautomation@users.noreply.github.com>
2019-03-20 12:26:55 -0400

    Oh wait ....

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("The WebHook Works - Building MSBUILD Jenkins Job to Test - AND THAT WORKED")
+        'MsgBox("GitHub Committ Triggered Visual Studio Build in Jenkins")
         Close()
     End Sub
 End Class
