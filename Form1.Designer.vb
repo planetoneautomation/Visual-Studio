7eb21b333db4efaf35b76374f724af085e2198da
planetoneautomation <48492716+planetoneautomation@users.noreply.github.com>
2019-03-21 13:23:56 -0400

    Moved Close button

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(938, 305)
+        Me.Button1.Location = New System.Drawing.Point(858, 301)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(136, 92)
         Me.Button1.TabIndex = 0
