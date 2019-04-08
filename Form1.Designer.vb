21eae8e7d5071056c1a586f5a8b0e3a2524c2852
planetoneautomation <48492716+planetoneautomation@users.noreply.github.com>
2019-04-08 09:54:50 -0400

    Resized and moved Close button

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,9 +28,10 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(863, 230)
+        Me.Button1.Location = New System.Drawing.Point(196, 99)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(141, 67)
+        Me.Button1.Size = New System.Drawing.Size(108, 53)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Close"
         Me.Button1.UseVisualStyleBackColor = True
@@ -39,19 +40,21 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(114, 60)
+        Me.Label1.Location = New System.Drawing.Point(57, 31)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(827, 55)
+        Me.Label1.Size = New System.Drawing.Size(429, 29)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "VS ---> GIT ---> Jenkins ---> Octane"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1056, 336)
+        Me.ClientSize = New System.Drawing.Size(528, 175)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "VS Integration with GIT"
         Me.ResumeLayout(False)
