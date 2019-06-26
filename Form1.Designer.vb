408aecbdebc7e1097deca08dcfecb93b9663f09e
planetoneautomation <48492716+planetoneautomation@users.noreply.github.com>
2019-06-26 15:30:28 -0400

    Close in middle

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,10 +28,10 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(37, 245)
+        Me.Button1.Location = New System.Drawing.Point(236, 201)
         Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(159, 79)
+        Me.Button1.Size = New System.Drawing.Size(529, 79)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Close"
         Me.Button1.UseVisualStyleBackColor = True
